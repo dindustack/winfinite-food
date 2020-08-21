e4a7b4529a06b9af2c00188f666896f17adbd016
ChinwenduSilvia <silviachinwendu@gmail.com>
2020-08-21 15:16:34 +0100

    Active state for navbar done

--- src/components/aboutPage.js
+++ src/components/aboutPage.js
@@ -1,181 +1,134 @@
-import React from 'react';
-import {
-    Link
-  } from "react-router-dom";
-import about from '../assets/img/about-one.jpg';
-import '../bootstrap.min.css';
-import '../index.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import about from "../assets/img/about-one.jpg";
+import "../bootstrap.min.css";
+import "../index.css";
 
 function AboutPage() {
-    return(
-        <React.Fragment>
-        {/* ----------- Bread Crumb ------- */}
-        <nav aria-label="breadcrumb" className="pt-5 mt-2 mt-md-5 mt-5 bg-white">
-            <div className="container"> 
-                <div className="row">
-                <div className="col-12">
-
-                   {/* BreadCrumb */}
-                    <ol className="breadcrumb mb-0 small text-muted bg-white">
-                    <li className="breadcrumb-item">
-                        <Link to="/" className="text-muted text-decoration-none">Home</Link>
-                    </li>
-                    <li className="breadcrumb-item active">
-                        About
-                    </li>
-                    </ol>
-
-                </div>
-                </div>
+  return (
+    <React.Fragment>
+      {/* ----------- Bread Crumb ------- */}
+      <nav aria-label="breadcrumb" className="pt-5 mt-2 mt-md-5 mt-5 bg-white">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              {/* BreadCrumb */}
+              <ol className="breadcrumb mb-0 small text-muted bg-white">
+                <li className="breadcrumb-item">
+                  <Link to="/" className="text-muted text-decoration-none">
+                    Home
+                  </Link>
+                </li>
+                <li className="breadcrumb-item active">About</li>
+              </ol>
             </div>
-        </nav>
-        <section className=" mb-5">
-            <div className="container">
+          </div>
+        </div>
+      </nav>
+      <section className=" mb-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center">
+              {/* ---- Heading ---- */}
+              <h3 className=" font-weight-bold heading display-4 mb-5 ">About</h3>
+            </div>
+          </div>
 
-                <div className="row">
-                <div className="col-12 text-center">
-
-                    {/* ---- Heading ---- */}
-                    <h3 className=" font-weight-bold heading display-4 mb-5 ">About</h3>
-                </div>
-                </div>
-
-                <div className="row align-items-center justify-content-around">
-                    {/* ---- About welcome image */}
-                    <div className="col-12 col-md-6 col-lg-6 order-md-2 mb-5">
-                        <img src={about} alt="about-cover" className="organic-radius img-fluid" />
-                    </div>
-
-                    {/* Introduction text */}
-                    <div className="col-12 col-md-6 text-center text-md-left col-lg-6 order-md-1 mb-5 mb-md-5">
-                    <h2 className="h1 heading">
-                        Superfood for everyone.
-                    </h2>
-                    
-                    {/* -- Text -- */}
-                    <p className="lead">
-                    Infinite Wealth Ltd. is an indigenous agro company born out of a 
-                    burning desire to respond to your basic health and cosmetic needs. 
-                    </p>
-                    <p className="lead">
-                    Our special focus is on paleo superfoods which are beneficial for 
-                    the skin and the stomach. Our firm belief is that if it is not good 
-                    enough for your precious digestive system, it is not good enough for your skin.
-
-                    </p>
-                    </div>
-
-                </div>
-
-                <div className="container mb-5">
-                    <div className="row text-center">
-                        <div className="col">
-                            <h2 className="h1 heading">Our Story</h2>
-                        </div>
-                    </div>
-                    <div className="row justify-content-center mt-5">
-                        <div className="col-md-12">
-
-                        
-                        <div className="timeline timeline-one">
-                        <div className="timeline-item"> 
-                            
-                            <h1 className="h2 mb-4 heading">Eggs and Chicken</h1>
-                            <p>
-                            In 2017, I closed my Nasarawa state based poultry business, 
-                            Infinite Wealth (now known as Winfinite Foods). 
-                            This wasn't easy; Infinite Wealth had been in existence 
-                            for over 8 years, birthed in the furnace of affliction. 
-                            Having been thrust by circumstances into an uncertain future. 
-
-                            </p>
-                            
-                        </div>
-                        <div className="timeline-item"> 
-                            
-                            <h1 className="heading h2 mb-4">Innovative plant-focused company</h1>
-                            <p>
-                            I and my sons, while discussing the Infinite Wealth journey, 
-                            had happened to mention the steady decline in the poultry 
-                            sector and the plant-based superfoods celebrated in Nigeria. 
-                            More and more people were adopting plant-based alternatives, 
-                            and eating healthier. She stopped and chose to pivot Infinite 
-                            Wealth into a new phase of superfoods production.  
-
-                            </p>
-                            
-                        </div>
-                        <div className="timeline-item"> 
-                            
-                            <h1 className="h2 mb-4 heading">An Idea turns to a business</h1>
-                            <p>
-                                The chickens had long since gone, but our mission 
-                                stayed the same: to create a sustainable and convenient 
-                                plant-focused food solution at an affordable price. 
-                                I preserved the iconic name but created a brand; WINFINITE FOODS .  
-                            </p>
-                            
-                        </div>
-                        <div className="timeline-item"> 
-                            
-                            <h1 className="h2 mb-4 heading">Women empowerment</h1>
-                            <p>
-                            We have created a network that empowers women from the 
-                            farms in the rural areas to the skyscrapers in the city. 
-                            We buy raw produce from female farmers and traders in 
-                            North-Central, North-West and South-Western Nigeria then 
-                            transport them to the beautiful city of Abuja where they 
-                            are processed into flour or powder under strict hygienic 
-                            conditions.
-  
-                            </p>
-                            
-                        </div>
-                        <div className="timeline-item"> 
-                            
-                            <h1 className="h2 mb-4 heading">Health and Wellness</h1>
-                            <p>
-                            The next process is the packaging which is done with love 
-                            for our clients and firm confidence in the knowledge that 
-                            we are sending out health and wellness. Finally, our 
-                            distribution chain consists primarily women who from the 
-                            proceeds, feed their families.
-
-  
-                            </p>
-                            
-                        </div>
-                        <div className="timeline-item"> 
-                            
-                            <h1 className="h2 mb-4 heading">Global inspiration</h1>
-                            <p>
-                            From Nigeria to the world aptly describes us as our 
-                            client base extends from the hills of Gwarinpa to the 
-                            silicon valleys of North America and from the scorching 
-                            heat of Lagos to the cold winters of Calgary. Women and 
-                            men across the world are beneficiaries of life’s wonders 
-                            birthed through us.
-
-                            </p>
-                            
-                        </div>
-                        
-
-                        </div>
-                        </div>
-
-                    </div>
-                    
-                </div>
-
+          <div className="row align-items-center justify-content-around">
+            {/* ---- About welcome image */}
+            <div className="col-12 col-md-6 col-lg-6 order-md-2 mb-5">
+              <img src={about} alt="about-cover" className="organic-radius img-fluid" />
             </div>
 
-        </section>
+            {/* Introduction text */}
+            <div className="col-12 col-md-6 text-center text-md-left col-lg-6 order-md-1 mb-5 mb-md-5">
+              <h2 className="h1 heading">Superfood for everyone.</h2>
 
-        </React.Fragment>
-    
-    ); 
+              {/* -- Text -- */}
+              <p className="lead">
+                Infinite Wealth Ltd. is an indigenous agro company born out of a burning desire to
+                respond to your basic health and cosmetic needs.
+              </p>
+              <p className="lead">
+                Our special focus is on paleo superfoods which are beneficial for the skin and the
+                stomach. Our firm belief is that if it is not good enough for your precious
+                digestive system, it is not good enough for your skin.
+              </p>
+            </div>
+          </div>
+
+          <div className="container mb-5">
+            <div className="row text-center">
+              <div className="col">
+                <h2 className="h1 heading">Our Story</h2>
+              </div>
+            </div>
+            <div className="row justify-content-center mt-5">
+              <div className="col-md-12">
+                <div className="timeline timeline-one">
+                  <div className="timeline-item">
+                    <h1 className="h2 mb-4 heading">Eggs and Chicken</h1>
+                    <p>
+                      In 2017, I closed my Nasarawa state based poultry business, Infinite Wealth
+                      (now known as Winfinite Foods). This wasn't easy; Infinite Wealth had been in
+                      existence for over 8 years, birthed in the furnace of affliction. Having been
+                      thrust by circumstances into an uncertain future.
+                    </p>
+                  </div>
+                  <div className="timeline-item">
+                    <h1 className="heading h2 mb-4">Innovative plant-focused company</h1>
+                    <p>
+                      I and my sons, while discussing the Infinite Wealth journey, had happened to
+                      mention the steady decline in the poultry sector and the plant-based
+                      superfoods celebrated in Nigeria. More and more people were adopting
+                      plant-based alternatives, and eating healthier. She stopped and chose to pivot
+                      Infinite Wealth into a new phase of superfoods production.
+                    </p>
+                  </div>
+                  <div className="timeline-item">
+                    <h1 className="h2 mb-4 heading">An Idea turns to a business</h1>
+                    <p>
+                      The chickens had long since gone, but our mission stayed the same: to create a
+                      sustainable and convenient plant-focused food solution at an affordable price.
+                      I preserved the iconic name but created a brand; WINFINITE FOODS .
+                    </p>
+                  </div>
+                  <div className="timeline-item">
+                    <h1 className="h2 mb-4 heading">Women empowerment</h1>
+                    <p>
+                      We have created a network that empowers women from the farms in the rural
+                      areas to the skyscrapers in the city. We buy raw produce from female farmers
+                      and traders in North-Central, North-West and South-Western Nigeria then
+                      transport them to the beautiful city of Abuja where they are processed into
+                      flour or powder under strict hygienic conditions.
+                    </p>
+                  </div>
+                  <div className="timeline-item">
+                    <h1 className="h2 mb-4 heading">Health and Wellness</h1>
+                    <p>
+                      The next process is the packaging which is done with love for our clients and
+                      firm confidence in the knowledge that we are sending out health and wellness.
+                      Finally, our distribution chain consists primarily women who from the
+                      proceeds, feed their families.
+                    </p>
+                  </div>
+                  <div className="timeline-item">
+                    <h1 className="h2 mb-4 heading">Global inspiration</h1>
+                    <p>
+                      From Nigeria to the world aptly describes us as our client base extends from
+                      the hills of Gwarinpa to the silicon valleys of North America and from the
+                      scorching heat of Lagos to the cold winters of Calgary. Women and men across
+                      the world are beneficiaries of life’s wonders birthed through us.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
 }
 
 export default AboutPage;
--- src/components/contactPage.js
+++ src/components/contactPage.js
@@ -1,115 +1,117 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import '../bootstrap.min.css';
-import '../index.css';
-
-
-
+import "../bootstrap.min.css";
+import "../index.css";
 
 function ContactPage() {
-    return(
-        <React.Fragment>
-            {/* ----------- Bread Crumb ------- */}
-            <nav aria-label="breadcrumb" className="pt-5 mt-2 mt-md-5 mt-5 bg-white">
-            <div className="container"> 
-                <div className="row">
-                <div className="col-12">
-
-                   {/* BreadCrumb */}
-                    <ol className="breadcrumb mb-0 small text-muted bg-white">
-                    <li className="breadcrumb-item">
-                        <Link to="/" className="text-muted text-decoration-none">Home</Link>
-                    </li>
-                    <li className="breadcrumb-item active">
-                       Contact
-                    </li>
-                    </ol>
-
-                </div>
-                </div>
+  return (
+    <React.Fragment>
+      {/* ----------- Bread Crumb ------- */}
+      <nav aria-label="breadcrumb" className="pt-5 mt-2 mt-md-5 mt-5 bg-white">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              {/* BreadCrumb */}
+              <ol className="breadcrumb mb-0 small text-muted bg-white">
+                <li className="breadcrumb-item">
+                  <Link to="/" className="text-muted text-decoration-none">
+                    Home
+                  </Link>
+                </li>
+                <li className="breadcrumb-item active">Contact</li>
+              </ol>
             </div>
-        </nav>
+          </div>
+        </div>
+      </nav>
 
-            {/* -- Contact Content -- */}
-            <section className="pt-5 pt-md-0 pb-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-
-                            {/* -- Heading -- */}
-                            <h3 className="mb-5 text-center display-4 heading">Contact Us</h3>
-
-                        </div>
-
-                    </div>
-                    <div className="row justify-content-between">
-
-                        {/* -------- Address ------------ */}
-                        <div className="col-12 col-md-6 mb-5">
-                            <div className="card shadow">
-                            <div className="card-body text-center">
-                            <svg viewBox="0 0 20 20" fill="#ff6f61" width="30px" className="mb-2">
-                            <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"/>
-                            </svg>
-                            <h3 className="mb-2 heading font-weight-bold">Business Address</h3>
-                            <p className="small text-muted font-weight-bold">29 Mambilla street  off Aso Drive Abuja, Nigeria</p>
-                            </div>
-                            </div>
-                        </div>
-
-                        {/* ------- Working hours ------------- */}
-                        <div className="col-12 col-md-6 mb-5">
-                            <div className="card shadow">
-                            <div className="card-body text-center">
-                            <svg viewBox="0 0 20 20" fill="#ff6f61" width="30px" className="mb-2">
-                            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd"/>                            
-                            </svg>
-                            <h3 className="mb-2 heading font-weight-bold">Working hours</h3>
-                            <h6 className="small text-muted font-weight-bold">Mon - Sun: 8AM - 5PM</h6>
-                            </div>
-                            </div>
-                        </div>
-
-                        {/* ------- Contact -------- */}
-                        <div className="col-12 col-md-6 mb-5">
-                            <div className="card shadow">
-                            <div className="card-body text-center">
-                            <svg viewBox="0 0 20 20" fill="#ff6f61" width="30px" className="mb-2">
-                            <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
-                            </svg>
-                            <h3 className="mb-2 heading font-weight-bold">Call us today</h3>
-                            <h6 className="small font-weight-bold">                                
-                            <a className="text-decoration-none text-muted" href="tel:+234-813-680-8501">+2348136808501</a>
-                            </h6>
-                            </div>
-                            </div>
-                        </div>
-
-                        {/* ------- Email -------- */}
-                        <div className="col-12 col-md-6 mb-5">
-                            <div className="card shadow">
-                            <div className="card-body text-center">
-                            <svg viewBox="0 0 20 20" fill="#ff6f61" width="30px" className="mb-2">
-                            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
-                            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/>                            
-                            </svg>
-                            <h3 className="mb-2 heading font-weight-bold">Send us a message</h3>
-                            <h6 className="small font-weight-bold">                                
-                            <a className="text-decoration-none text-muted" href="mailto:info@winfinitefoods.com">info@winfinitefoods.com</a>
-                            </h6>
-                            </div>
-                            </div>
-                        </div>
-
-                    </div>
+      {/* -- Contact Content -- */}
+      <section className="pt-5 pt-md-0 pb-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              {/* -- Heading -- */}
+              <h3 className="mb-5 text-center display-4 heading">Contact Us</h3>
+            </div>
+          </div>
+          <div className="row justify-content-between">
+            {/* -------- Address ------------ */}
+            <div className="col-12 col-md-6 mb-5">
+              <div className="card shadow">
+                <div className="card-body text-center">
+                  <svg viewBox="0 0 20 20" fill="#ff6f61" width="30px" className="mb-2">
+                    <path
+                      fillRule="evenodd"
+                      d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <h3 className="mb-2 heading font-weight-bold">Business Address</h3>
+                  <p className="small text-muted font-weight-bold">
+                    29 Mambilla street off Aso Drive Abuja, Nigeria
+                  </p>
                 </div>
+              </div>
+            </div>
 
-            </section>
+            {/* ------- Working hours ------------- */}
+            <div className="col-12 col-md-6 mb-5">
+              <div className="card shadow">
+                <div className="card-body text-center">
+                  <svg viewBox="0 0 20 20" fill="#ff6f61" width="30px" className="mb-2">
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <h3 className="mb-2 heading font-weight-bold">Working hours</h3>
+                  <h6 className="small text-muted font-weight-bold">Mon - Sun: 8AM - 5PM</h6>
+                </div>
+              </div>
+            </div>
 
-           
-          
-        </React.Fragment>
-    );
+            {/* ------- Contact -------- */}
+            <div className="col-12 col-md-6 mb-5">
+              <div className="card shadow">
+                <div className="card-body text-center">
+                  <svg viewBox="0 0 20 20" fill="#ff6f61" width="30px" className="mb-2">
+                    <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                  </svg>
+                  <h3 className="mb-2 heading font-weight-bold">Call us today</h3>
+                  <h6 className="small font-weight-bold">
+                    <a className="text-decoration-none text-muted" href="tel:+234-813-680-8501">
+                      +2348136808501
+                    </a>
+                  </h6>
+                </div>
+              </div>
+            </div>
+
+            {/* ------- Email -------- */}
+            <div className="col-12 col-md-6 mb-5">
+              <div className="card shadow">
+                <div className="card-body text-center">
+                  <svg viewBox="0 0 20 20" fill="#ff6f61" width="30px" className="mb-2">
+                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                  </svg>
+                  <h3 className="mb-2 heading font-weight-bold">Send us a message</h3>
+                  <h6 className="small font-weight-bold">
+                    <a
+                      className="text-decoration-none text-muted"
+                      href="mailto:info@winfinitefoods.com">
+                      info@winfinitefoods.com
+                    </a>
+                  </h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
 }
 
 export default ContactPage;
